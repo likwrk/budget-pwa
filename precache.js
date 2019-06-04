27ea073a3c39a6109a3466d612204e8e7ad13823
likwrk <likwrk@gmail.com>
2019-06-04 07:45:27 +0300

    remove .map from precache

--- precache.js
+++ precache.js
@@ -2,7 +2,11 @@ const swPrecache = require('sw-precache');
 const rootDir = './public/';
 swPrecache.write(`${rootDir}service-worker.js`, {
   staticFileGlobs: [
-    rootDir + '*',
+    rootDir + '*.css',
+    rootDir + '*.js',
+    rootDir + '*.json',
+    rootDir + '*.xml',
+    rootDir + '*.ico',
   ],
   stripPrefix: rootDir
 }, () => { process.exit(0); });
